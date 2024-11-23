e893b9b8911a8c0a528d2c164230cb33791a0a58
JarrStore <163243032+JarrStore@users.noreply.github.com>
2024-11-23 22:10:59 +0700

    Update uninstal.sh

--- uninstal.sh
+++ uninstal.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+# Menghapus website dan file terkait
+echo "Menghapus website dari /var/www/html/samp_website..."
+rm -rf /var/www/html/samp_website
+
+# Menghapus file database admin.sql
+echo "Menghapus file database admin.sql..."
+rm -f /var/www/html/samp_website/admin.sql
+
+echo "Website dan database telah dihapus."
